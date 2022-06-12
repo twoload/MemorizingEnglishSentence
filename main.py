cb1bab0435a415ce576e562c00c8585f01c7f5c4
twoload <wangyoung.sohn@gmail.com>
2022-06-12 21:14:49 +0900

    add: myPdf.py

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
-import pandas as pd
 from openpyxl import load_workbook
-from fpdf import FPDF
-#from excel import Excel
-
-class PDF(FPDF):
-    def header(self):
-        # Arial bold 15
-        #self.set_font('Arial', 'B', 15)
-        # Move to the right
-        #self.cell(80)
-        # Title
-        #self.cell(30, 10, title, 1, 0, 'C')
-        # Line break
-        self.ln(20)
-
-    # Page footer
-    def footer(self):
-        # Position at 1.5 cm from bottom
-        self.set_y(-15)
-        # Arial italic 8
-        self.set_font('Arial', 'I', 8)
-        # Page number
-        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
-
-
-def isWorkSheet(excelname, worksheet):
-    wb = load_workbook(excelname, read_only=True)
-    if worksheet in wb.sheetnames:
-        #print(f'exist {worksheet}')
-        return True
-    else:
-        #print(f'no exist {worksheet}')
-        return False
+from myExcel import *
+from myPdf import *
 
 def inputSubTitlePage(index):
     if isWorkSheet('mainPage.xlsx', str(index)):
         print(f'there is worksheet {str(index)}')
-        subPage = pd.read_excel('mainPage.xlsx', str(index))
+        subPage = excel2dataframe('mainPage.xlsx', str(index))
     else:
         print(f'there is no worksheet {str(index)}')
         return
@@ -96,7 +65,7 @@ def inputSubTitlePage(index):
 
 
 def inputMainPage():
-    mainPage = pd.read_excel('mainPage.xlsx', 'mainPage')
+    mainPage = excel2dataframe('mainPage.xlsx', 'mainPage')
     print(mainPage)
 
     choose = 0
@@ -146,7 +115,7 @@ def inputMainPage():
     return
 
 def study():
-    mainPage = pd.read_excel('mainPage.xlsx', 'mainPage')
+    mainPage = excel2dataframe('mainPage.xlsx', 'mainPage')
 
     while 1:
         studyMenu = 0
@@ -164,54 +133,21 @@ def study():
             print('select index >')
             index = int(input())
             if isWorkSheet('mainPage.xlsx', str(index)):
-                subPage = pd.read_excel('mainPage.xlsx', str(index))
+                subPage = excel2dataframe('mainPage.xlsx', str(index))
             print(subPage)
 
             nameTitle = mainPage['title'][index] + '___' + mainPage['subTitle'][index]
-            pdf = FPDF()
+            pdf = MY_PDF()
             pdf.add_page()
-            pdf.add_font('sysfont', '', 'NEXONLv1GothicRegular.ttf')
-            pdf.set_font("sysfont", size=10)
-            line_height = pdf.font_size * 2.5
+            pdf.myEnv()
+            pdf.myHead(nameTitle)
+            pdf.myBody_2columes('English', 'Korean', subPage)
+            pdf.mySaveToFile(f'{nameTitle}.pdf')
 
-            # head
-            pdf.set_font(style = 'U')
-            pdf.cell(w = 0, h = 10, txt = nameTitle, border = 0, align = 'C')
-            pdf.set_font(style ='')
-            pdf.ln(line_height*2)
-            # contents
-            col_width = pdf.epw / 2  # distribute content evenly
-            for index in subPage.index:
-                eng = subPage['English'][index]
-                kor = subPage['Korean'][index]
-                data_row = (eng, kor)
-                for datum in data_row:
-                    pdf.multi_cell(col_width, line_height, datum, border=0, new_x="RIGHT", new_y="TOP", max_line_height=pdf.font_size)
-                pdf.ln(line_height)
-
-            pdf.output(f'{nameTitle}.pdf')
         elif studyMenu == int(0):
             break
         else:
             print('wrong input')
-
-
-'''
-            # create pdf
-            pdf = FPDF()
-            pdf.add_page()
-            pdf.set_font('Arial', '', 16)
-            pdf = FPDF('P', 'mm', 'A4')
-            #pdf.title(mainPage['title'][index], mainPage['subTitle'][index])
-            nameTitle = mainPage['title'][index] + mainPage['subTitle'][index]
-            pdf.set_title('nameTitle')
-            pdf.cell(40, 10, 'hello')
-            #for index in subPage.index:
-                #print(subPage['English'][index], subPage['Korean'][index])
-            #    pdf.cell(40, 10, subPage['English'][index])
-            #    pdf.cell()
-            pdf.output('output.pdf', 'F')
-    '''
 
 def showWelcome():
     print("==========================")
